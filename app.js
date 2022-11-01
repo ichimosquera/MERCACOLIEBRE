ddb8428cf2f5633d9782e8c6941c8e7dbdb95056
Irene Mosquera <irenemosquera@gmail.com>
2022-11-01 20:31:29 -0300

    Mercado Liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static('public'));
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>console.log('Servidor corriendo en puerto' + PORT))
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {  
+    res.sendFile(path.resolve('./views/home.html'));
+  });
+
+  app.get('/register', function (req, res) {
+    res.sendFile(path.resolve('./views/register.html'));
+  });
+
+
+
+
